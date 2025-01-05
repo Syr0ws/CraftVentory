63747f6867524511d0539db2525eec70a3ec10d7
Syrows <clement.gho@gmail.com>
2025-01-05 17:02:25 +0100

    adding javadoc

--- src/main/java/com/github/syr0ws/craftventory/common/util/CommonContextKey.java
+++ src/main/java/com/github/syr0ws/craftventory/common/util/CommonContextKey.java
@@ -2,9 +2,28 @@ package com.github.syr0ws.craftventory.common.util;
 
 public class CommonContextKey {
 
+    /**
+     * Key for {@link com.github.syr0ws.craftventory.api.inventory.InventoryViewer}
+     */
     public static final String VIEWER = "VIEWER";
+
+    /**
+     * Key for {@link com.github.syr0ws.craftventory.api.inventory.CraftVentory}
+     */
     public static final String INVENTORY = "INVENTORY";
+
+    /**
+     * Key for an inventory slot.
+     */
     public static final String SLOT = "SLOT";
+
+    /**
+     * Key for a pagination id.
+     */
     public static final String PAGINATION_ID = "PAGINATION_ID";
-    public static final String PAGINATED_DATA = "PAGINATION_ITEM";
+
+    /**
+     * Key for a paginated data inside a pagination.
+     */
+    public static final String PAGINATED_DATA = "PAGINATED_DATA";
 }
