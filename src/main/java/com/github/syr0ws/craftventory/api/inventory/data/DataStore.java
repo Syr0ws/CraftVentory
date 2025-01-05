7149e32685c1daf780ff9b8f1b39eefbeb47f349
Syr0ws <45719267+Syr0ws@users.noreply.github.com>
2025-01-05 18:28:27 +0100

    docs: update DataStore javadoc (#62)

--- src/main/java/com/github/syr0ws/craftventory/api/inventory/data/DataStore.java
+++ src/main/java/com/github/syr0ws/craftventory/api/inventory/data/DataStore.java
@@ -4,17 +4,13 @@ import java.util.Optional;
 import java.util.Set;
 
 /**
- * Represents a data model associated with an inventory.
- * <p>
- * The {@link DataStore} provides a flexible way to store, retrieve, and manage
- * data associated with an inventory instance. It supports typed data storage
- * and retrieval based on unique keys.
- * </p>
+ * Represents a container used to store data. Each data is associated with a unique
+ * key and its Java class type.
  */
 public interface DataStore {
 
     /**
-     * Stores a data entry in the inventory model.
+     * Adds a new data represented as a key-value pair to the store.
      *
      * @param key   The unique key that identifies the data entry. Must not be {@code null} or empty.
      * @param type  The {@link Class} type of the data. Must not be {@code null}.
@@ -24,7 +20,7 @@ public interface DataStore {
     <T> void setData(String key, Class<T> type, T value);
 
     /**
-     * Removes a data entry from the inventory model by its key.
+     * Removes a data from the store.
      *
      * @param key The unique key that identifies the data entry to remove. Must not be {@code null}.
      * @return {@code true} if the data entry was successfully removed, {@code false} otherwise.
@@ -32,12 +28,12 @@ public interface DataStore {
     boolean removeData(String key);
 
     /**
-     * Removes all the stored data in the storage.
+     * Removes all the stored data from the store.
      */
     void clear();
 
     /**
-     * Checks if the inventory model contains a data entry for the specified key.
+     * Checks if the store contains a data associated with the given key.
      *
      * @param key The unique key that identifies the data entry. Must not be {@code null}.
      * @return {@code true} if a data entry exists for the given key, {@code false} otherwise.
@@ -45,7 +41,7 @@ public interface DataStore {
     boolean hasData(String key);
 
     /**
-     * Checks if the inventory model contains a data entry for the specified key and class.
+     * Checks if the store contains a data of the given type associated with the given key.
      *
      * @param key The unique key that identifies the data entry. Must not be {@code null}.
      * @param type The {@link Class} type of the data. Must not be {@code null}.
@@ -54,7 +50,7 @@ public interface DataStore {
     boolean hasData(String key, Class<?> type);
 
     /**
-     * Retrieves a data entry from the inventory model.
+     * Retrieves a data from the store by key.
      *
      * @param key  The unique key that identifies the data entry. Must not be {@code null}.
      * @param type The {@link Class} type of the data. Must not be {@code null}.
@@ -66,7 +62,7 @@ public interface DataStore {
     <T> Optional<T> getData(String key, Class<T> type);
 
     /**
-     * Retrieves all the keys currently stored in the inventory model.
+     * Retrieves all the currently stored keys.
      *
      * @return An immutable {@link Set} containing all the keys of the stored data entries.
      * Never {@code null}.
